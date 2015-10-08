beb7a4e9c2b18e7a43a346d4eabe71d8af06fe91
Uldis Sturms <uldis.sturms@gmail.com>
2015-10-08 23:18:51 +0100

    Support run from mono

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
-.paket/paket.bootstrapper.exe
-rm paket.lock
-.paket/paket.exe update
+#!/bin/bash
+if test "$OS" = "Windows_NT"
+then
+  # use .Net
+  .paket/paket.bootstrapper.exe
+  rm paket.lock
+  .paket/paket.exe update
+else
+  # use mono
+  mono .paket/paket.bootstrapper.exe
+  rm paket.lock
+  mono .paket/paket.exe update
+fi
+
 for subdir in packages/*; do
     (cd "$subdir"; echo "Running: ${PWD##*/}"; ./run.sh)
 done
